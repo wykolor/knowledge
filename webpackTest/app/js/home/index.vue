30041cefd2b102f63d53a5528fd0e9e9a4eb1761
wykolor <kolor1121460147@163.com>
2019-05-30 16:40:57 +0800

    webpack基础配置

---
+++ webpackTest/app/js/home/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="home">
+        <h1>我是标题
+            <p class="title">我是小标题 </p>
+        </h1>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang='css'>
+.home{
+    color: pink;
+    font-size: 20px;
+}
+
+</style>
