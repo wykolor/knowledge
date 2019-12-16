2571f6f93da9743cf22666c15f098f8cc387e502
wykolor <kolor1121460147@163.com>
2019-12-16 17:32:03 +0800

    虚拟DOM实现

--- 1211diffDom/js/index.js
+++ 1211diffDom/js/index.js
@@ -1,17 +1,22 @@
 /*
  * @Author: your name
  * @Date: 2019-12-11 17:40:44
- * @LastEditTime: 2019-12-11 18:05:52
+ * @LastEditTime: 2019-12-12 10:23:58
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \DHAPPd:\knowledge\1211diffDom\js\index.js
  */
 
-let virtualDom = createElement("ul", { class: "list" }, [
-  createElement("li", { class: "list-item" }, ["a"]),
-  createElement("li", { class: "list-item" }, ["b"]),
-  createElement("li", { class: "list-item" }, ["c"])
-]);
+let virtualDom = createElement(
+  "ul",
+  { class: "list", style: "color:red;font-size:16px;" },
+  [
+    createElement("li", { class: "list-item" }, ["a"]),
+    createElement("li", { class: "list-item" }, ["b"]),
+    createElement("li", { class: "list-item" }, ["c"])
+  ]
+);
 let el = render(virtualDom);
+renderDom(el, window.app);
 console.log(virtualDom);
 console.log(el);
